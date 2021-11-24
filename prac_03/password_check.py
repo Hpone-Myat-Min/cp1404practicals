6ab5caa6380727eee93387856a9c55c820e0bb7a
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2021-11-24 15:22:19 +0630

    Add docstring to functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -2,11 +2,13 @@ MIN_PASSWORD_LENGTH = 7
 
 
 def main():
+    """Check password."""
     password = get_password()
     print_asterisks(password)
 
 
 def get_password():
+    """Get valid password."""
     password = input("Enter password: ")
     while len(password) < MIN_PASSWORD_LENGTH:
         print("Password must be at least", MIN_PASSWORD_LENGTH)
@@ -15,6 +17,7 @@ def get_password():
 
 
 def print_asterisks(password):
+    """Print as many asterisks as password length"""
     print("*" * len(password))
 
 
