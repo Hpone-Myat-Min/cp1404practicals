a52e3dab844978624561913215e5ff2969856894
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2021-11-24 14:33:37 +0630

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -6,16 +6,16 @@ def main():
     print_asterisks(password)
 
 
-def print_asterisks(password):
-    print("*" * len(password))
-
-
 def get_password():
     password = input("Enter password: ")
     while len(password) < MIN_PASSWORD_LENGTH:
         print("Password must be at least", MIN_PASSWORD_LENGTH)
         password = input("Enter password: ")
     return password
+
+
+def print_asterisks(password):
+    print("*" * len(password))
 
 
 main()
