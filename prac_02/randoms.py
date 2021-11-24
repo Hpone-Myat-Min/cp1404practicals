cc817c004201ddd5fa41b25eaac68950bdda53d6
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2021-11-24 12:57:49 +0630

    Add random number generator

---
+++ prac_02/randoms.py
@@ -0,0 +1,19 @@
+# What did you see on line 1?
+#   I see 20.
+# What was the smallest number you could have seen, what was the largest?
+#   Smallest number = 5 and largest number = 20
+
+# What did you see on line 2?
+#   I see 5.
+# What was the smallest number you could have seen, what was the largest?
+#   Smallest number = 3 and largest number = 9
+# Could line 2 have produced a 4?
+#   No, line 2 cannot produced 4.
+
+# What did you see on line 3?
+#     I see 2.9844543487137987
+# What was the smallest number you could have seen, what was the largest?
+#     Smallest number = 2.5 and largest number = 5.5
+
+import random
+print(random.randint(1, 100))
