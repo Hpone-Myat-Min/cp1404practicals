2ecb786768e3ecbb12ea6bd1fbedb0c6c5a2ecdb
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2021-11-24 15:07:03 +0630

    Add a function to determine score status

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,15 +3,31 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 # TODO: Fix this!
+import random
 MIN_EXCELLENT_SCORE = 90
 MIN_PASSING_SCORE = 50
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
+
+
+def main():
+    """Determine score status."""
     score = float(input("Enter score: "))
-if score >= MIN_EXCELLENT_SCORE:
-    print("Excellent")
-elif score >= MIN_PASSING_SCORE:
-    print("Passable")
-else:
-    print("Bad")
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    score_status = determine_score_status(score)
+    print(score_status)
+    random_score = random.randint(0, 100)
+    print(determine_score_status(random_score))
+
+
+def determine_score_status(score):
+    """Check which category score falls in."""
+    if score >= MIN_EXCELLENT_SCORE:
+        return "Excellent"
+    elif score >= MIN_PASSING_SCORE:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
