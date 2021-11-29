fe80f29eadfae9a824c64d003d5dca5479f7b303
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2021-11-29 20:27:40 +0630

    Correct is_valid_password function

--- prac_02/password_checker.py
+++ prac_02/password_checker.py
@@ -4,7 +4,7 @@ Password checker "skeleton" code to help you get started
 """
 
 MIN_LENGTH = 2
-MAX_LENGTH = 6
+MAX_LENGTH = 15
 SPECIAL_CHARS_REQUIRED = False
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
@@ -12,13 +12,12 @@ SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 def main():
     """Program to get and check a user's password."""
     print("Please enter a valid password")
-    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
-          "characters, and contain:")
+    print("Your password must be between {} and {} characters, and contain:".format(MIN_LENGTH, MAX_LENGTH))
     print("\t1 or more uppercase characters")
     print("\t1 or more lowercase characters")
     print("\t1 or more numbers")
     if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+        print("\tand 1 or more special characters: {}".format(SPECIAL_CHARACTERS))
     password = input("> ")
     while not is_valid_password(password):
         print("Invalid password!")
@@ -45,17 +44,17 @@ def is_valid_password(password):
         elif char.isdigit():
             count_digit += 1
 
-    # TODO: if any of the 'normal' counts are zero, return False
-    if count_digit == 0:
+    # # TODO: if any of the 'normal' counts are zero, return False
+    if count_lower == 0 or count_upper == 0 or count_digit == 0:
         return False
-    if count_lower == 0:
-        return False
-    if count_upper == 0:
-        return False
-    # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
-    if SPECIAL_CHARS_REQUIRED and SPECIAL_CHARACTERS not in password:
-        if SPECIAL_CHARACTERS not in password:
+    #
+    # # TODO: if special characters are required, then check the count of those
+    # # and return False if it's zero
+    if SPECIAL_CHARS_REQUIRED:
+        for char in password:
+            if char in SPECIAL_CHARACTERS:
+                count_special += 1
+        if count_special == 0:
             return False
     # if we get here (without returning False), then the password must be valid
     return True
