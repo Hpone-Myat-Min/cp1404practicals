6a0bcd78f393c9217249f958eabf187106d29394
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2021-12-28 10:51:49 +0630

    Add init method

---
+++ prac_06/programming_language.py
@@ -0,0 +1,10 @@
+class ProgrammingLanguage:
+
+    def __init__(self, language, typing, reflection, year):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
+
