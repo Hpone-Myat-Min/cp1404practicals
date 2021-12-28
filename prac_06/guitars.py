78c7aa2f5e8b6373474338bcc99e18ffd8fe03f9
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2021-12-28 12:53:47 +0630

    Display details of all guitars.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -17,4 +17,14 @@ while not blank_name:
         guitar = Guitar(name, year, cost)
         guitars.append(guitar)
 
+""" Testing """
+# guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+# guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
+print("\nThese are my guitars:")
+for i, guitar in enumerate(guitars, 1):
+    if guitar.is_vintage():
+        vintage_string = " (vintage)"
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+    else:
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}")
