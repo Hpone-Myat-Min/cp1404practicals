97ae194a69a22ed053ede90f210b6293dbf31580
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2021-12-28 10:54:50 +0630

    Add is_dynamic function in ProgramminLanguage class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -6,5 +6,9 @@ class ProgrammingLanguage:
         self.reflection = reflection
         self.year = year
 
-
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
 
