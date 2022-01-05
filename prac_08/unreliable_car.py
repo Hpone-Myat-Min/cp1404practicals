bd9c30570195f5cebeceedb411afa9abbc3e8ded
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2022-01-05 11:39:43 +0630

    Add a str method in UnreliableCar class

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -7,11 +7,15 @@ class UnreliableCar(Car):
     def __init__(self, name='', fuel=0, reliability=0):
         super().__init__(name, fuel)
         self.reliability = reliability
+        self.distance_driven = 0
+
+    def __str__(self):
+        return f"{super().__str__()}, distance driven: {self.distance_driven}"
 
     def drive(self, distance):
-        distance = random.randint(0, 100)
-        if distance < self.reliability:
+        if random.randint(0, 100) < self.reliability * 100:
             distance_driven = super().drive(distance)
         else:
             distance_driven = 0
+        self.distance_driven = distance_driven
         return distance_driven
