bf81b8bddd795b97dcfa1baa4bd6e1dc35046918
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2022-01-05 13:51:47 +0630

    Refactor SilverServiceTaxi Class

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -2,21 +2,16 @@ from taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
-    flagfall = 4.5
 
-    def __init__(self):
-        super().__init__(name='', fuel=0)
-        self.fanciness = 0
+    flagfall = 4.50
+
+    def __init__(self, name='', fuel=0, fanciness=0):
+        super().__init__(name, fuel)
+        self.fanciness = float(fanciness)
         self.price_per_km = Taxi.price_per_km * self.fanciness
 
     def __str__(self):
-        return f"{super().__str__()} plus flagfall of ${SilverServiceTaxi.flagfall}"
+        return f"{super().__str__()} plus flagfall of ${self.flagfall}"
 
     def calculate_fare(self):
-        """ Calculate the fare."""
-        return super().get_fare() + SilverServiceTaxi.flagfall
-
-
-
-
-
+        return float(super().get_fare()) + self.flagfall
