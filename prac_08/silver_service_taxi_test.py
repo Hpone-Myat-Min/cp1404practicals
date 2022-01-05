b56763c5c8c840cf1f940a7f2d9e56b64651d4ed
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2022-01-05 13:55:16 +0630

    Test SilverServiceTaxi class

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,15 @@
+from silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    taxi_1 = SilverServiceTaxi("Hummer", 200, 4)
+    print(taxi_1)
+
+    taxi_2 = SilverServiceTaxi("Harrier", 200, 2)
+    taxi_2.drive(18)
+
+    print(f"Fare for an 18 km trip with fanciness of 2: ${taxi_2.calculate_fare():.2f}")
+
+
+if __name__ == '__main__':
+    main()
