23b7fc5d365fb9b1bd0f3a61165e53edc826b76b
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2022-01-11 09:02:47 +0630

    Fix typo error

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -8,7 +8,7 @@ def main():
     taxi_2 = SilverServiceTaxi("Harrier", 200, 2)
     taxi_2.drive(18)
 
-    print(f"Fare for an 18 km trip with fanciness of 2: ${taxi_2.calculate_fare():.2f}")
+    print(f"Fare for an 18 km trip with fanciness of 2: ${taxi_2.get_fare():.2f}")
 
 
 if __name__ == '__main__':
